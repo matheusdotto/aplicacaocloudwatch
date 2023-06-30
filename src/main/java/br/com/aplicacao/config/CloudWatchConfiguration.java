a477b953ae4cf8075eb94cc503875ce3068b8f68
Matheus Otto <matheusotto@live.com>
2023-06-30 16:22:59 -0300

    adicionando botstrap

--- src/main/java/br/com/aplicacao/config/CloudWatchConfiguration.java
+++ src/main/java/br/com/aplicacao/config/CloudWatchConfiguration.java
@@ -4,6 +4,7 @@ import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.logs.AWSLogs;
 import com.amazonaws.services.logs.AWSLogsClientBuilder;
 import org.springframework.context.annotation.Bean;
@@ -17,6 +18,7 @@ public class CloudWatchConfiguration {
     private final String secretKey = "12345";
 
 
+
     public AWSCredentials credentials() {
         AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
         return credentials;
@@ -26,7 +28,7 @@ public class CloudWatchConfiguration {
     public AWSLogs awsLogs() {
 
         AWSLogs cloudWatchlog = AWSLogsClientBuilder.standard()
-                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://host.docker.internal:4566", "us-east-1"))
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://localhost:4566", "us-east-1"))
                 .withCredentials(new AWSStaticCredentialsProvider(credentials()))
                 .build();
         return cloudWatchlog;
